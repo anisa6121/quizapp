ee632c15965471b0ec960a65d4befda5d949576c
anisa6121 <you@example.com>
2022-10-11 20:30:10 +0600

    img-add

--- src/components/Herosection/Herosection.js
+++ src/components/Herosection/Herosection.js
@@ -9,22 +9,24 @@ const Herosection = () => {
 						<h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
 							Anytime Anywhere
 							<br className="hidden md:block" />
-                            Learn on your
-                            <br />
+							Learn on your
+							<br />
 							<span className="inline-block text-deep-purple-accent-400">
 								Suitable Schedule
 							</span>
 						</h2>
 						<p className="text-base text-gray-700 md:text-lg ">
-							Find your favorite subject select <br /> and practice. It will increase your skills and knowledge.
+							Find your favorite subject
+							select <br /> and practice. It
+							will increase your skills and
+							knowledge.
 						</p>
 					</div>
-                    <hr className="mb-6 border-gray-300" />
-                </div>
+					<hr className="mb-6 border-gray-300" />
+				</div>
 
 				<div className="px-5 pt-6 pb-5 text-center border border-gray-300 rounded lg:w-3/5">
-					
-					
+					<img src="images/tiny.jpg" alt="" />
 				</div>
 			</div>
 		</div>
