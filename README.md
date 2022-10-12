b26203a45081a3457f4c356daf003639b2a9d061
anisa6121 <you@example.com>
2022-10-12 20:11:37 +0600

    chart-complete

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 
 - Description 
- -1. First i create some component in my Project.
--2.Then add Navbar. And complete it.
+ 1. First i create some component in my Project.
+ -2. Then add Navbar. And complete it.
 
-- 3. Project Title and favicon add. Data Load and display in ui. 
+ - 3. Project Title and favicon add. Data Load and display in ui. 
  - 4.And then i do the work of other routes complete.
 
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,10 +1,41 @@
-import React from 'react';
-
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import {BarChart,Bar,Cell,} from "recharts";
 const Chart = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://openapi.programming-hero.com/api/quiz/2")
+
+.then((data) => {
+ const loadData = data.data.data.questions
+const getData = loadData.map(data =>{
+
+    const dataId = parseInt(data.id);
+ const SingleData = {
+	id: dataId,
+	Answer: data.correctAnswer,
+    };
+    
+    return SingleData
+})
+    
+    setData(getData)
+console.log(loadData)
+
+});
+       
+
+    },[])
+    
     return (
-        <div>
-            <h1>This is chart component</h1>
-        </div>
+        <>
+         
+	<BarChart width={500} height={600} data={data} className='lg:m-auto md:m-auto'>
+	<Bar dataKey="id" fill="#8884d8" />
+	</BarChart>
+		</>
     );
 };
 
